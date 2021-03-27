5708923a8c8c61c4d5d9c12ad381fba7f0f473f4
anaclaudiacomuna <81341643+anaclaudiacomuna@users.noreply.github.com>
2021-03-27 02:51:58 -0300

    Delete Docs Directory

--- docs/assets/js/numeroRandomico.js
+++
@@ -1,1 +0,0 @@
-function numeroAleatorio(){var e=Math.floor(1e3*Math.random());document.getElementById("number").innerHTML=e}$(function(){document.getElementById("generate-number").addEventListener("click",numeroAleatorio)});
